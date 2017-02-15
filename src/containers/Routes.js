1d09bbdef0261d9affd66bf1cf10931204ce6e94
jeffreyfriedman <jeff@eurodollaryen.com>
2017-02-14 21:26:38 -0500

    WIP converting to CSS-based movement

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -3,6 +3,7 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { history } from '../store';
 import App from './App';
 import Game from './Game';
+import Alternative from './Alternative';
 import About from './About';
 
 export const Routes =
@@ -10,6 +11,7 @@ export const Routes =
     <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={Game}/>
+        <Route path="/alternative" component={Alternative}/>
         <Route path="/about" component={About}/>
       </Route>
     </Router>
