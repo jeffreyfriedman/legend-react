000d92b27bb70bc4801e7a404eb3fde8351f5502
Jeffrey Friedman <jeff@eurodollaryen.com>
2017-03-11 11:38:04 -0500

    Update Game.js

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -95,7 +95,7 @@ export default class Game extends Component {
       lastMove = 'down';
     }
 
-    if (this.state.keyState[90]) {  // z
+    if (this.state.keyState['KeyZ] || || this.state.keyState['z']) {  // z
       lastMove = 'swordAttack';
     }
 
